4dc6447c84ac9b662af0c9e6d1b23ef5fb8f29f2
Miles France <miles.france@gmail.com>
2015-11-17 14:25:03 -0700

    more updates - added modules

--- scripts/composer/post-install.sh
+++ scripts/composer/post-install.sh
@@ -7,6 +7,19 @@ if [ ! -f web/sites/default/settings.php ]
     chmod 777 web/sites/default/settings.php
 fi
 
+echo "\$databases['default']['default'] = array (
+  'database' => 'drupal',
+  'username' => 'drupal',
+  'password' => 'drupal',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+\$settings['install_profile'] = 'zap';
+\$config_directories['sync'] = 'sites/default/files/config_4eJmMXr6BeZgx7dcsXSb9SDTZ6HzGJnNpyJia3ymYnLKtS_xdLdg_f3A4GB2HNJDg3BWE2ODNQ/sync';" >> web/sites/default/settings.php
+
 # Prepare the services file for installation
 if [ ! -f web/sites/default/services.yml ]
   then
@@ -19,3 +32,17 @@ if [ ! -d web/sites/default/files ]
   then
     mkdir -m777 web/sites/default/files
 fi
+# Prepare the modules directory for development.
+if [ ! -d web/modules/custom ]
+  then
+    mkdir web/modules/custom
+fi
+if [ ! -d web/modules/features ]
+  then
+    mkdir web/modules/features
+fi
+if [ ! -d web/modules/sandbox ]
+  then
+    mkdir web/modules/sandbox
+fi
+find . -name "README.txt" -type f|xargs rm -f
